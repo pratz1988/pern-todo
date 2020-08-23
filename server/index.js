a7848e233eee7122b48749c9a3b171d7bd4e6b31
pratz1988 <prathyusha.jaddu@gmail.com>
2020-08-23 12:26:16 -0400

    Building Routes

--- server/index.js
+++ server/index.js
@@ -7,6 +7,31 @@ const pool = require("./db");
 app.use(cors());
 app.use(express.json());
 
+//ROUTES
+
+//create a todo
+app.post("/todos", async (req, res) => {
+  try {
+    console.log(req.body);
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1)",
+      [description]
+    );
+    res.json(newTodo);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//Get all todos
+
+// Get a todo
+
+//Update a todo
+
+//Delete a todo
+
 app.listen(5000, () => {
   console.log("Server has started on port  5000");
 });
